2ba544b0bc97a45994d164bc42d592f66ed556b9
Jeff Wang <wrh003769@163.com>
2017-08-07 00:44:34 +0800

    New dataset
    training set (500, 36865, 10) * 4 files
    
    testing set (35, 36865, 10)

--- try_mat/try_mat.py
+++ try_mat/try_mat.py
@@ -6,10 +6,10 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 
 data = sio.loadmat('my2data(1).mat')
-test_data = sio.loadmat('new_my2data.mat')
+#test_data = sio.loadmat('new_my2data.mat')
 
 print(data['pover'].shape)
-print(test_data['pover'].shape)
+#print(test_data['pover'].shape)
 
 
 #for i in range(test_data['pover'].shape[0]):
@@ -18,13 +18,13 @@ print(test_data['pover'].shape)
 #sio.savemat('new_my3data.mat', {'pover': test_data['pover']})
 
 train_x = np.asarray([np.reshape(x, (192,192,10)) for x in data['pover'][:, 0:-1, :]])
-test_x = np.asarray([np.reshape(x, (192,192,10)) for x in test_data['pover'][:, 0:-1, :]])
-train_y = np.reshape(data['pover'][:, -1, 0], (data['pover'].shape[0]))
+#test_x = np.asarray([np.reshape(x, (192,192,10)) for x in test_data['pover'][:, 0:-1, :]])
+#train_y = np.reshape(data['pover'][:, -1, 0], (data['pover'].shape[0]))
 
-plt.subplot(121)
-plt.imshow(train_x[69, :, :, 0])
-plt.subplot(122)
-plt.imshow(test_x[69, :, :, 0])
+#plt.subplot(121)
+plt.imshow(train_x[12, :, :, 0])
+#plt.subplot(122)
+#plt.imshow(test_x[69, :, :, 0])
 plt.show()
 
 #print(train_x.shape)
